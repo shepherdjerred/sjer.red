1d1dba5d548ee0cd03398d5c6ce46b0e5a486c4f
Jerred Shepherd <shepherdjerred@gmail.com>
2022-10-05 22:51:06 -0700

    redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Blog from "./components/Blog";
 import BlogList from "./components/BlogList";
@@ -12,13 +12,13 @@ if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <GlobalStyles />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="*" element={<App />}></Route>
           <Route path="/blog" element={<BlogList />}></Route>
           <Route path="/blog/libvirt-and-c" element={<Blog />}></Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   );
 }
