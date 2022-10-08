8aa9d15a28b3a41228bdbe734490a25c459cea24
Jerred Shepherd <shepherdjerred@gmail.com>
2022-10-08 00:50:33 -0700

    fix react

--- src/components/ChaosText.tsx
+++ src/components/ChaosText.tsx
@@ -1,9 +1,8 @@
-import "react";
 import ChaosLetter from "./ChaosLetter";
 
 export default function ChaosText({ text }: { text: string }) {
   const chaosText = text.split("").map((char) => {
     return <ChaosLetter key={char}>{char}</ChaosLetter>;
   });
-  return <>{chaosText}</>;
+  return <span>{chaosText}</span>;
 }
