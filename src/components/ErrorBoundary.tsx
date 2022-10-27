09bc8fb3ae1c4bf02009c1f1976e9655826f7a47
Jerred Shepherd <shepherdjerred@gmail.com>
2022-10-27 00:03:38 -0700

    add error boundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import tw from "twin.macro";
+
+export interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: unknown) {
+    return { hasError: true };
+  }
+
+  override render() {
+    if (this.state.hasError) {
+      return (
+        <h1 css={tw`font-mono place-self-center`}>Something went wrong.</h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import BlogList from "./pages/BlogList";
 import Home from "./pages/Home";
 import { PostRouter } from "./posts/posts";
@@ -11,14 +12,16 @@ const root = document.getElementById("root");
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="/blog" element={<BlogList />} />
-          <Route path="/blog/*" element={<PostRouter />} />
-        </Routes>
-      </BrowserRouter>
+      <ErrorBoundary>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<Home />} />
+            <Route path="/blog" element={<BlogList />} />
+            <Route path="/blog/*" element={<PostRouter />} />
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
     </React.StrictMode>
   );
 }
