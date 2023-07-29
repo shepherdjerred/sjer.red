799dffd35f0b1ba68a17c1a358592d21f42465b3
Jerred Shepherd <github@sjer.red>
2023-07-29 12:45:00 -0700

    Remove old files

--- src/components/ChaosText.tsx
+++
@@ -1,9 +0,0 @@
-import ChaosLetter from "./ChaosLetter";
-import React from "react";
-
-export default function ChaosText({ text }: { text: string }) {
-  const chaosText = text.split("").map((char) => {
-    return <ChaosLetter key={char}>{char}</ChaosLetter>;
-  });
-  return <React.Fragment>{chaosText}</React.Fragment>;
-}
