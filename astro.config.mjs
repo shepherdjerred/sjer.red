ceb580902f0e31bb4a1ca12b33f242b6450bdf9f
Jerred Shepherd <github@sjer.red>
2023-09-08 18:25:22 -0700

    clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +13,5 @@ export default defineConfig({
     },
   },
   site: "https://sjer.red",
-  integrations: [mdx(), sitemap(), tailwind(), prefetch()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
