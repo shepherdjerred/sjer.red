71d6b31f81615d9a42fba2b698d23f132b8b7564
Jerred Shepherd <github@sjer.red>
2024-01-20 10:20:54 -0800

    add links

---
+++ src/links.ts
@@ -0,0 +1,13 @@
+export const links = [
+  "https://awesomekling.github.io/Excellence-is-a-habit-but-so-is-failure/",
+  "https://diary.geekodour.org/",
+  "https://www.morling.dev/blog/the-code-review-pyramid/",
+  "https://hyperboleandahalf.blogspot.com/2013/05/depression-part-two.html",
+  "https://ludic.mataroa.blog/blog/your-organization-probably-doesnt-want-to-improve-things/",
+  "https://milan.cvitkovic.net/writing/things_youre_allowed_to_do/",
+  "https://parhelia.conorbarnes.com/p/the-placeholder-girlfriend",
+  "https://usefulfictions.substack.com/p/how-to-be-more-agentic",
+  "https://sashachapin.substack.com/p/things-you-learn-dating-cate-hall",
+  "https://www.epicweb.dev/fully-typed-web-apps",
+  "https://vinnie.dev/using-zod-at-the-edge-of-our-typescript-type-system/",
+];
