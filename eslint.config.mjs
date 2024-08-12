5d7d9fc6a1d767e46bd84c6b9cce8d91fa1d57a1
Jerred Shepherd <github@sjer.red>
2024-08-12 11:24:28 -0700

    fix: enable project service

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default [
   {
     languageOptions: {
       parserOptions: {
-        project: true,
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
