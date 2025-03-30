d4122a37ab0c63770a0d1ed44d506dd05f1dab6b
Jerred Shepherd <github@sjer.red>
2025-03-30 10:58:33 -0700

    fix: hn link

--- src/content/schemas/index.ts
+++ src/content/schemas/index.ts
@@ -10,12 +10,12 @@ export const BasePostSchema = z.object({
 
 export const BlogSchema = BasePostSchema.extend({
   image: z.string().optional(),
-  hackerNews: z.string().optional(),
+  hackerNews: z.string().url().optional(),
 }).transform((val) => ({ ...val, description: val.description ?? val.title, type: "blog" }));
 
 export const TilSchema = BasePostSchema.extend({
   title: z.string().transform((val) => `TIL: ${val}`),
-  hackerNews: z.string().optional(),
+  hackerNews: z.string().url().optional(),
 }).transform((val) => ({ ...val, description: val.description ?? val.title, type: "til" }));
 
 export const LeetCodeSchema = BasePostSchema.transform((val) => ({
